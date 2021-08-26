8b93df95adb4111281dec219d2d2b79e68f26446
pre5ent666 <75908533+pre5ent666@users.noreply.github.com>
2021-08-26 16:48:13 +0800

    Update Results_Summary.py

--- Results_Summary.py
+++ Results_Summary.py
@@ -2,35 +2,43 @@ from openpyxl import load_workbook, workbook
 from openpyxl import Workbook
 import re
 
-def Results_Sum(wb, ws, Title):
-    row_title = ['Original GM TC ID', Title]
-    ws.append(row_title)
+def Results_Sum(wb, ws, name):
     j = 0
     for sheet in wb:
         print()
         i = 0
         if sheet.title == 'Case need update':
             break
-        for row in sheet.iter_rows(min_row=2, max_col=2, values_only=True):
+        for row in sheet.iter_rows(min_row=2, max_col=7, values_only=True):
             if not row[0] is None:
                 ws.append(row)
                 i = i + 1
                 j = j + 1
         print(sheet.title + ': ' + str(i))
-    print('======================== ' + Title + ' Total:' + str(j) + ' ========================')
+    print('======================== ' + name + ' Total:' + str(j) + ' ========================')
 
 
 #--------------------------------------------- Input Settings -------------------------------------------------
-Week = 30
+Week = 35
 folder = 'C:\\Users\\Yvonne\\Documents\\Results\\'
-Order = ['Main', 'Production', 'STR']
+Order = ['Main', 'STR', 'SORP']
+Auto = [0, 1, 1]
 #--------------------------------------------------------------------------------------------------------------
 
 
 output = Workbook()
-for Line_Name in Order:
+for k in range(3):
+    print(k)
+    Line_Name = Order[k]
     Title = 'W' + str(Week) + '_' + Line_Name
     wb =  load_workbook(folder + '\\Sorted\\' + Title +'_Sorted.xlsx')
     ws = output.create_sheet(Line_Name)
-    Results_Sum(wb, ws, Title)
+    row_title = ['Original GM TC ID', Title]
+    ws.append(row_title)
+    Results_Sum(wb, ws, Title + '_Man')
+
+    if Auto[k] == 1:
+        wa =  load_workbook(folder + '\\Sorted\\' + Title +'_Auto.xlsx')
+        Results_Sum(wa, ws, Title + '_Auto')
+
 output.save(folder + '\\All\\2021_W' + str(Week) + '.xlsx')
